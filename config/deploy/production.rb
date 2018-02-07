4ff915b5f7ad3af1f8702ab909cbbbc1f3af6ce1
Szymon Sobczak <szymon.sobczak@gmail.com>
2018-02-07 10:43:22 -0800

    02.07

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "wordpress-ss", user: "ssobczak", roles: %w{app static}
+server "wordpress", roles: %w{app static}
 
 
 # role-based syntax
