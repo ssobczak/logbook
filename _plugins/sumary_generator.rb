3216f3a47e671debe3dc625dde09cf7830ff219a
Szymon Sobczak <szymon.sobczak@gmail.com>
2018-03-06 14:28:20 -0800

    Add title in sumary.

--- _plugins/sumary_generator.rb
+++ _plugins/sumary_generator.rb
@@ -7,7 +7,8 @@ module Jekyll
       @pages << {
         collection: page.collection.label,
         date: page.data['date'].strftime('%F'),
-        url: @site.config['url'] + page.url
+        url: @site.config['url'] + page.url,
+        title: page.data['title']
       }
     end
 
